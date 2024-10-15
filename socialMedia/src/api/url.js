82b478e7c646efb699fd6f1de03828d5744964ec
Radiant <aks251102@gmail.com>
2024-10-15 09:08:18 +0630

    deleting login no needed

--- socialMedia/src/api/url.js
+++ socialMedia/src/api/url.js
@@ -4,9 +4,10 @@ export const base_api_url = "http://localhost:8080";
 
 const jwtToken = localStorage.getItem("jwt");
 
-export const api = axios.create({baseURL: base_api_url,
-    headers:{
-        "Authorization" : `Bearer ${jwtToken}`,
-        "Content-Type" : "application/json"
+export const api = axios.create({
+    baseURL: base_api_url,
+    headers: {
+        "Authorization": `Bearer ${jwtToken}`,
+        "Content-Type": "application/json"
     }
-})
+});
